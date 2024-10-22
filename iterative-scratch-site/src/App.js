aa935621758309be336a21c0a5ff555e7bf0acf9
scoggs-erasolutions <dcscoggins@gmail.com>
2024-10-22 14:33:49 -0400

    search parent added

--- iterative-scratch-site/src/App.js
+++ iterative-scratch-site/src/App.js
@@ -1,26 +1,33 @@
-import "./App.module.css";
-import axios from "axios";
-import { useState } from "react";
-import CardDefault from "./components/CardDefault";
+// import axios from 'axios';
+// import { useState } from 'react';
+// import RecipeCard from './components/RecipeCard/RecipeCard';
+import styles from './App.module.css';
+import SearchCardRandom from './components/SearchCardRandom/SearchCardRandom';
 
 function App() {
-  const [card, setCard] = useState(null);
+  // const [card, setCard] = useState(null);
 
-  const getRecipe = async () => {
-    try {
-      let response = await axios.get(
-        `https://api.spoonacular.com/recipes/random?apiKey=1700ab22148d4c39a98a22bfa3f36c38`
-      );
-      setCard(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  // const getRecipe = async () => {
+  //   try {
+  //     let response = await axios.get(
+  //       `https://api.spoonacular.com/recipes/random?apiKey=1700ab22148d4c39a98a22bfa3f36c38`
+  //     );
+  //     setCard(response.data);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
 
   return (
-    <div>
-      <button onClick={getRecipe}>Get Random Recipe</button>
-      <CardDefault props={card} />
+    <div className={styles.section}>
+      <SearchCardRandom />
+      <SearchCardRandom />
+      <SearchCardRandom />
+      <SearchCardRandom />
+      <SearchCardRandom />
+      <SearchCardRandom />
+      <SearchCardRandom />
+      <SearchCardRandom />
     </div>
   );
 }
