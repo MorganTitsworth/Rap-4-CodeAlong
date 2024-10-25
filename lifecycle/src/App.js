a96e00e6a8dda461c5183956882eccc0db8e97f5
Morgan <m.titsworth@erasolutions.us>
2024-10-25 11:56:24 -0700

    useRef

--- lifecycle/src/App.js
+++ lifecycle/src/App.js
@@ -1,18 +1,20 @@
-import logo from './logo.svg';
 import styles from './App.module.css';
-import SearchParent from './components/searchParent/SearchParent';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 function App() {
   const [count, setCount] = useState(0);
-
-  console.log('rendering app.js')
+  const countRef = useRef(0);
   return (
     <div className={styles.App}>
       <header className={styles.Appheader}>
-      <input type="text" onChange={(e) => {
-                setCount(count + 1);
-            }} />
-        <SearchParent props={count}/>
+        <button onClick={() => setCount(count + 1)}>Increase count State</button>
+        <br />
+        <button onClick={() => {
+          countRef.current = countRef.current + 1;
+          console.log('countRef.current', countRef.current)
+        }
+        }>Increase count Ref</button>
+       <p> REF: {countRef.current} STATE: {count}</p>
+        {countRef.current === 5 && 'asdasdasd'}
       </header>
     </div>
   );
