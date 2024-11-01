35a5c25b6b8132c28810de33fe2703cbe3c48f76
Morgan <m.titsworth@erasolutions.us>
2024-11-01 12:24:21 -0700

    layoutEffect

---
+++ lifecycle/src/components/layoutEffectComponent/LayoutEffectComponent.js
@@ -0,0 +1,26 @@
+import { useLayoutEffect, useEffect, useRef, useState } from 'react';
+
+export default function LayoutEffectComponent() {
+    const elementRef = useRef(null);
+    const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+    useEffect(() => {
+        if (elementRef.current) {
+            const { width, height } = elementRef.current.getBoundingClientRect();
+            setDimensions({ width, height });
+        }
+    }, []);
+
+    return (
+        <div style={{
+            width: '100%',
+            height: '100vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+        }} ref={elementRef}>
+            <div style={{ width: '100%' }}>
+                Width: {dimensions.width}, Height: {dimensions.height}
+            </div>
+        </div>
+    );
+}
